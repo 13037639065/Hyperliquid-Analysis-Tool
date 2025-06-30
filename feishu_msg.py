0ec759dd8b196c13e2908df3c4fc80a5b2f5b013
wurong <wurong@autoxing.com>
2025-06-30 18:10:54 +0800

    add

--- feishu_msg.py
+++ feishu_msg.py
@@ -1,7 +1,7 @@
 import requests
 import os
 import json
-
+import time
 def send_feishu_text(title, content):
     headers = {"Content-Type": "application/json"}
     payload = {
@@ -15,3 +15,10 @@ def send_feishu_text(title, content):
         response = requests.post(webhook_url,  headers=headers, data=json.dumps(payload)) 
     except Exception as e:
         print(f"[send_feishu_text] error: {e}")
+
+# 
+
+if __name__ == "__main__":
+    date_time_str = time.strftime("%Y-%m-%dT%H:%M:%S", time.localtime())
+    msg = f"{date_time_str} 发送成功"
+    send_feishu_text("测试一下", msg)
