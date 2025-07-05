8526f06ce067ae800b2a819a96fbd0e401ce4db9
wurong <wurong@autoxing.com>
2025-07-06 07:33:56 +0800

    add guard

--- hyper2binance.py
+++ hyper2binance.py
@@ -34,10 +34,9 @@ def hyper_log(message, level="info"):
     if level == "error":
         send_feishu_text(f"机器人错误报警", message)
 
-if __name__ == "__main__":
+def main():
     send_feishu_text("交易机器人", "自动交易机器人启动成功")
     order_id_map = {}
-    
     
     binance_client = UMFutures(key=os.environ.get("binance_api_key"), secret=os.environ.get("binance_api_secret"))
     exchange_info = binance_client.exchange_info()
@@ -207,3 +206,13 @@ if __name__ == "__main__":
     websocket_url = "wss://api.hyperliquid.xyz/ws"
     ws = websocket.WebSocketApp(websocket_url, on_message=on_message, on_open=on_open)
     ws.run_forever(ping_interval=30, ping_timeout=10, reconnect=10)
+
+
+if __name__ == "__main__":
+    while True:
+        try:
+            main()
+        except KeyboardInterrupt:
+            hyper_log("KeyboardInterrupt will exit...", "error")
+        except Exception as e:
+            hyper_log(f"An error occurred, will restart bot: {e}", "error")
