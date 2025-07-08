5aa8a2fe77a22a5a004c2c9c69dca2bce5da5410
wurong <wurong@autoxing.com>
2025-07-08 11:56:42 +0800

    fix log

--- hyer_util.py
+++ hyer_util.py
@@ -1,6 +1,9 @@
 # util functions for Hyperliquid analysis tool
+import os
+import __main__
 
-def hyper_file_log(message, path="trading_data_cache/hyer_log.txt"):
+def hyper_file_log(message):
+    base_path = "trading_data_cache/logs"
     """
     将日志信息追加写入指定文件
     
@@ -8,5 +11,8 @@ def hyper_file_log(message, path="trading_data_cache/hyer_log.txt"):
         message (str): 要写入的信息
         path (str): 日志文件路径，默认为 hyer_log.txt
     """
-    with open(path, "a", encoding="utf-8") as f:
+    os.makedirs(base_path, exist_ok=True)
+    pid = os.getpid()
+    procoss_name = os.path.basename(__main__.__file__).replace(".py", "")  # 获取主进程文件名
+    with open(os.path.join(base_path, f"{procoss_name}_{pid}.log"), "a", encoding="utf-8") as f:
         f.write(f"{message}\n")
